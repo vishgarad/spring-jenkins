85920add7520587b767319880671b3bcefa77ecc
Vishal Garad <garad_v@India.XoriantCorp.com>
2021-09-30 11:54:59 +0530

    jre changes

--- src/main/java/com/jenkins/SpringJenkinsApplication.java
+++ src/main/java/com/jenkins/SpringJenkinsApplication.java
@@ -6,19 +6,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SpringJenkinsApplication {
-	public static Logger logger=LoggerFactory.getLogger(SpringJenkinsApplication.class);
-	
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
 	@PostConstruct
-	public void intt()
-	{
+	public void intt() {
 		logger.info("application started ");
 	}
 
 	public static void main(String[] args) {
-		
+
 		logger.info("application ececuted  ");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
